6bb68df1732fd4a9ae5e46c54262f5616d99b918
rohan20 <rhntnj@gmail.com>
2023-06-17 19:22:45 +0200

    add ConferenceBaseDataModel

---
+++ lib/features/conference_base/data/model/conference_base_data_model.dart
@@ -0,0 +1,32 @@
+import 'package:fluttercon/features/conference_base/data/model/session_model.dart';
+import 'package:fluttercon/features/conference_base/data/model/speaker_model.dart';
+
+class ConferenceBaseDataModel {
+  ConferenceBaseDataModel({
+    this.sessions = const [],
+    this.speakers = const [],
+  });
+
+  factory ConferenceBaseDataModel.fromJson(Map<String, dynamic> json) {
+    return ConferenceBaseDataModel(
+      sessions: json['sessions'] != null
+          ? (json['sessions'] as List<dynamic>)
+              .map<SessionModel>((sessionJson) => SessionModel.fromJson(sessionJson as Map<String, dynamic>))
+              .toList()
+          : [],
+      speakers: json['speakers'] != null
+          ? (json['speakers'] as List<dynamic>)
+              .map<SpeakerModel>((speakerJson) => SpeakerModel.fromJson(speakerJson as Map<String, dynamic>))
+              .toList()
+          : [],
+    );
+  }
+
+  final List<SessionModel> sessions;
+  final List<SpeakerModel> speakers;
+
+  @override
+  String toString() {
+    return 'ConferenceBaseDataModel(sessions: $sessions, speakers: $speakers)';
+  }
+}
