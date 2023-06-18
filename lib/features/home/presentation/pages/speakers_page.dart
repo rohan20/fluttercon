ddd38aa3c920c47b88ac8227f9710b9a76d73418
rohan20 <rhntnj@gmail.com>
2023-06-18 23:01:49 +0200

    use CachedNetworkImage to display the speaker's image

--- lib/features/home/presentation/pages/speakers_page.dart
+++ lib/features/home/presentation/pages/speakers_page.dart
@@ -1,7 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:conference_data/conference_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttercon/features/home/presentation/bloc/bloc.dart';
+
+const _speakerImageRadius = 24.0;
 
 class SpeakersPage extends StatelessWidget {
   const SpeakersPage({super.key});
@@ -66,23 +69,7 @@ class _SpeakersListItem extends StatelessWidget {
       color: backgroundColor,
       child: Row(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(color: Theme.of(context).colorScheme.primary),
-            ),
-            // On background image error, use the first character of the speakers' name
-            child: CircleAvatar(
-              radius: 24,
-              foregroundImage: NetworkImage(speaker.profilePictureUrl),
-              child: Text(
-                speaker.fullName[0],
-                style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-              ),
-            ),
-          ),
+          _SpeakerImage(name: speaker.fullName, imageUrl: speaker.profilePictureUrl),
           const SizedBox(width: 16),
           Expanded(
             child: Column(
@@ -93,7 +80,7 @@ class _SpeakersListItem extends StatelessWidget {
                   style: Theme.of(context).textTheme.titleSmall,
                 ),
                 if (speaker.tagLine.isNotEmpty) ...{
-                  const SizedBox(height: 8),
+                  const SizedBox(height: 4),
                   Text(
                     speaker.tagLine,
                     style: Theme.of(context).textTheme.bodySmall,
@@ -103,6 +90,56 @@ class _SpeakersListItem extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _SpeakerImage extends StatelessWidget {
+  const _SpeakerImage({
+    required this.name,
+    required this.imageUrl,
+  });
+
+  final String name;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(color: Theme.of(context).colorScheme.primary),
+      ),
+      child: CachedNetworkImage(
+        imageUrl: imageUrl,
+        imageBuilder: (_, imageProvider) {
+          return CircleAvatar(
+            radius: _speakerImageRadius,
+            backgroundImage: imageProvider,
+          );
+        },
+        placeholder: (_, __) => _SpeakerImageFallback(name: name),
+        errorWidget: (_, __, ___) => _SpeakerImageFallback(name: name),
+      ),
+    );
+  }
+}
+
+class _SpeakerImageFallback extends StatelessWidget {
+  const _SpeakerImageFallback({
+    required this.name,
+  });
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: _speakerImageRadius,
+      child: Text(
+        name[0],
+        style: Theme.of(context).textTheme.headlineSmall!.copyWith(color: Theme.of(context).colorScheme.primary),
       ),
     );
   }
