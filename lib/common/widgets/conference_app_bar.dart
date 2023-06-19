fbeeb87a22fb16b71769fa4641e3c20d071a93e1
rohan20 <rhntnj@gmail.com>
2023-06-19 23:33:23 +0200

    extract ConferenceAppBar

---
+++ lib/common/widgets/conference_app_bar.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ConferenceAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const ConferenceAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: SvgPicture.asset(
+        'assets/images/fluttercon.svg',
+        width: min(MediaQuery.of(context).size.width * 0.4, 150),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
