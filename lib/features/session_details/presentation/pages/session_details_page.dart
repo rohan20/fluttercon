8560d8cde0d3986fdeb6dddfed39a0899b2c322d
rohan20 <rhntnj@gmail.com>
2023-06-19 23:35:35 +0200

    add a basic SessionDetailsPage showing session title, desc and speakers

---
+++ lib/features/session_details/presentation/pages/session_details_page.dart
@@ -0,0 +1,51 @@
+import 'package:conference_data/conference_data.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttercon/common/widgets/conference_app_bar.dart';
+import 'package:fluttercon/common/widgets/speaker_list_item.dart';
+
+class SessionDetailsPage extends StatelessWidget {
+  const SessionDetailsPage({
+    required this.session,
+    required this.speakers,
+    required this.categories,
+    required this.roomName,
+    super.key,
+  });
+
+  final Session session;
+  final List<Speaker> speakers;
+  final List<Category> categories;
+  final String roomName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const ConferenceAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                session.title,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 8),
+              for (final speaker in speakers) ...{
+                SpeakersListItem(
+                  speaker: speaker,
+                  backgroundColor: Colors.transparent,
+                ),
+              },
+              const SizedBox(height: 8),
+              Text(
+                session.description,
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
