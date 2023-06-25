88b8f647aa18c4f04f56f85e9211d0a929bb2109
rohan20 <rhntnj@gmail.com>
2023-06-25 02:04:39 +0200

    append agenda sessions to GetConferenceDataUseCase's result

--- packages/conference_data/lib/src/domain/use_case/get_conference_data_use_case.dart
+++ packages/conference_data/lib/src/domain/use_case/get_conference_data_use_case.dart
@@ -1,5 +1,6 @@
 import 'package:conference_data/src/di/injector.dart';
 import 'package:conference_data/src/domain/entity/conference_data.dart';
+import 'package:conference_data/src/domain/entity/session.dart';
 import 'package:conference_data/src/domain/repository/conference_data_repository.dart';
 import 'package:util/util.dart';
 
@@ -10,14 +11,27 @@ class GetConferenceDataUseCase extends UseCase<ConferenceData, NoParams> {
   Future<Result<Failure, ConferenceData>> call([NoParams? params]) async {
     final conferenceDataRepository = injector.get<ConferenceDataRepository>();
 
+    // TODO(rohan20): Parallelize these calls
     final conferenceDataResult = await conferenceDataRepository.getConferenceData();
+    final agendaResult = await conferenceDataRepository.getAgenda();
 
     if (conferenceDataResult.isError()) {
       return Error(conferenceDataResult.getError()!);
     } else {
       final conferenceData = conferenceDataResult.getSuccess()!;
 
-      return Success(conferenceData);
+      // List<Session> from [ConferenceData] is just a list of talks.
+      // List<Session> from [Agenda] is the same list of talks plus breaks, registration, lunch, etc. events.
+      final agendaSessions = agendaResult.when(
+        (_) => <Session>[],
+        (agenda) => agenda.sessions,
+      );
+
+      if (agendaSessions.isEmpty) {
+        return Success(conferenceData);
+      }
+
+      return Success(conferenceData.copyWith(sessions: agendaSessions));
     }
   }
 }
