982075bddf99afaf6ca2aeba789b786f9a0177c5
rohan20 <rhntnj@gmail.com>
2023-06-25 03:27:03 +0200

    fix parsing session-category logic

--- packages/conference_data/lib/src/data/model/agenda_model.dart
+++ packages/conference_data/lib/src/data/model/agenda_model.dart
@@ -6,13 +6,28 @@ class AgendaModel {
   });
 
   factory AgendaModel.fromJson(List<dynamic> jsonList) {
+    final sessionModelsList = jsonList
+        .map<List<dynamic>>((agendaDay) => (agendaDay as Map<String, dynamic>)['rooms'] as List<dynamic>)
+        .expand((List<dynamic> roomJson) => roomJson)
+        .expand((roomJson) => (roomJson as Map<String, dynamic>)['sessions'] as List<dynamic>)
+        .map<SessionModel>(
+      (sessionJson) {
+        final categoryItemJsonList = ((sessionJson as Map<String, dynamic>)['categories'] as List<dynamic>)
+            .map((categoryJson) => categoryJson as Map<String, dynamic>)
+            .expand((categoryJson) => categoryJson['categoryItems'] as List<dynamic>)
+            .map((categoryItemJson) => (categoryItemJson as Map<String, dynamic>)['id'])
+            .toList();
+
+        sessionJson
+          ..putIfAbsent('categoryItems', () => categoryItemJsonList)
+          ..remove('categories');
+
+        return SessionModel.fromJson(sessionJson);
+      },
+    ).toList();
+
     return AgendaModel(
-      sessions: jsonList
-          .map<List<dynamic>>((agendaDay) => (agendaDay as Map<String, dynamic>)['rooms'] as List<dynamic>)
-          .expand((List<dynamic> roomJson) => roomJson)
-          .expand((roomJson) => (roomJson as Map<String, dynamic>)['sessions'] as List<dynamic>)
-          .map<SessionModel>((sessionJson) => SessionModel.fromJson(sessionJson as Map<String, dynamic>))
-          .toList(),
+      sessions: sessionModelsList,
     );
   }
 
