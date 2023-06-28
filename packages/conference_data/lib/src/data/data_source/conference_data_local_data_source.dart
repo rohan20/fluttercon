ac329c0c1d0a5917b1ef049c02b9072cfe3b6ad8
rohan20 <rhntnj@gmail.com>
2023-06-28 12:53:53 +0200

    add local data source impl for caching conference data

--- packages/conference_data/lib/src/data/data_source/conference_data_local_data_source.dart
+++ packages/conference_data/lib/src/data/data_source/conference_data_local_data_source.dart
@@ -1,8 +1,12 @@
 import 'package:conference_data/src/data/model/agenda_model.dart';
 import 'package:conference_data/src/data/model/conference_data_model.dart';
 
-abstract class ConferenceDataRemoteDataSource {
+abstract class ConferenceDataLocalDataSource {
+  Future<void> saveConferenceData(ConferenceDataModel conferenceDataModel);
+
   Future<ConferenceDataModel> getConferenceData();
+
+  Future<void> saveAgenda(AgendaModel agendaModel);
 
   Future<AgendaModel> getAgenda();
 }
--- packages/conference_data/lib/src/data/data_source/conference_data_local_data_source_impl.dart
+++ packages/conference_data/lib/src/data/data_source/conference_data_local_data_source_impl.dart
@@ -1,12 +1,63 @@
+import 'dart:convert';
+
+import 'package:conference_data/src/data/data_source/conference_data_local_data_source.dart';
 import 'package:conference_data/src/data/model/agenda_model.dart';
 import 'package:conference_data/src/data/model/conference_data_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-abstract class ConferenceDataLocalDataSource {
-  Future<void> saveConferenceData(ConferenceDataModel conferenceDataModel);
+const _conferenceDataKey = 'conferenceData';
+const _agendaKey = 'agenda';
 
-  Future<ConferenceDataModel> getConferenceData();
+class ConferenceDataLocalDataSourceImpl implements ConferenceDataLocalDataSource {
+  ConferenceDataLocalDataSourceImpl(this.sharedPreferences);
 
-  Future<void> saveAgenda(AgendaModel agendaModel);
+  final SharedPreferences sharedPreferences;
 
-  Future<AgendaModel> getAgenda();
+  @override
+  Future<void> saveConferenceData(ConferenceDataModel conferenceDataModel) async {
+    try {
+      await sharedPreferences.setString(_conferenceDataKey, jsonEncode(conferenceDataModel.toJson()));
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<ConferenceDataModel> getConferenceData() async {
+    try {
+      final conferenceDataJson = sharedPreferences.getString(_conferenceDataKey);
+
+      if (conferenceDataJson == null) {
+        throw Exception('No cached conference data found');
+      }
+
+      return ConferenceDataModel.fromJson(jsonDecode(conferenceDataJson) as Map<String, dynamic>);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<void> saveAgenda(AgendaModel agendaModel) async {
+    try {
+      await sharedPreferences.setString(_agendaKey, jsonEncode(agendaModel.toJson()));
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<AgendaModel> getAgenda() async {
+    try {
+      final agendaJson = sharedPreferences.getString(_agendaKey);
+
+      if (agendaJson == null) {
+        throw Exception('No cached agenda found');
+      }
+
+      return AgendaModel.fromJson(jsonDecode(agendaJson) as List<dynamic>);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
 }
