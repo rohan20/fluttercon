63513716fbee06b60102944c3063367b3ca21512
rohan20 <rhntnj@gmail.com>
2023-06-28 15:28:16 +0200

    implement remote vs local logic

--- packages/conference_data/lib/src/domain/repository/conference_data_repository_impl.dart
+++ packages/conference_data/lib/src/domain/repository/conference_data_repository_impl.dart
@@ -1,26 +1,70 @@
 import 'package:api_client/api_client.dart';
+import 'package:conference_data/src/data/data_source/conference_data_local_data_source.dart';
 import 'package:conference_data/src/data/data_source/conference_data_remote_data_source.dart';
+import 'package:conference_data/src/domain/conference_data_source.dart';
 import 'package:conference_data/src/domain/entity/agenda.dart';
 import 'package:conference_data/src/domain/entity/conference_data.dart';
 import 'package:conference_data/src/domain/repository/conference_data_repository.dart';
 import 'package:util/util.dart';
 
 class ConferenceDataRepositoryImpl implements ConferenceDataRepository {
-  ConferenceDataRepositoryImpl({required this.conferenceDataRemoteDataSource});
+  ConferenceDataRepositoryImpl({
+    required this.conferenceDataRemoteDataSource,
+    required this.conferenceDataLocalDataSource,
+  });
 
   final ConferenceDataRemoteDataSource conferenceDataRemoteDataSource;
+  final ConferenceDataLocalDataSource conferenceDataLocalDataSource;
 
+  // TODO(rohan20): Simplify with just a single try-catch that uses private exceptions which are rethrown.
+  // TODO(rohan20): Remove print statements.
   @override
-  Future<Result<Failure, ConferenceData>> getConferenceData() async {
+  Future<Result<Failure, ConferenceData>> getConferenceData({
+    ConferenceDataSource source = ConferenceDataSource.cached,
+  }) async {
     try {
-      final conferenceDataModel = await conferenceDataRemoteDataSource.getConferenceData();
+      try {
+        if (source == ConferenceDataSource.cached) {
+          print('XXX Getting cached conference data...');
+          final cachedConferenceDataModel = await conferenceDataLocalDataSource.getConferenceData();
 
-      return Success(ConferenceData.fromConferenceDataModel(conferenceDataModel));
+          if (cachedConferenceDataModel.sessions.isNotEmpty) {
+            print('XXX Returning latest conference data because source = cached');
+            return Success(ConferenceData.fromConferenceDataModel(cachedConferenceDataModel));
+          } else {
+            print('XXX Returning latest conference data because cached sessions were empty');
+            return Success(await _getLatestConferenceDataAndCacheIt());
+          }
+        } else {
+          print('XXX Returning latest conference data because source = latest');
+          return Success(await _getLatestConferenceDataAndCacheIt());
+        }
+      } on ApiClientError {
+        print('XXX Error ApiClientError 1');
+        rethrow;
+      } catch (e) {
+        print('XXX Returning latest conference data since something went wrong: $e');
+        return Success(await _getLatestConferenceDataAndCacheIt());
+      }
     } on ApiClientError {
+      print('XXX Error ApiClientError 2');
       return const Error(ServerFailure());
-    } catch (_) {
+    } catch (e) {
+      print('XXX Error $e');
       return const Error(LocalFailure());
     }
+  }
+
+  Future<ConferenceData> _getLatestConferenceDataAndCacheIt() async {
+    final conferenceDataModel = await conferenceDataRemoteDataSource.getConferenceData();
+
+    try {
+      await conferenceDataLocalDataSource.saveConferenceData(conferenceDataModel);
+    } catch (_) {
+      return ConferenceData.fromConferenceDataModel(conferenceDataModel);
+    }
+
+    return ConferenceData.fromConferenceDataModel(conferenceDataModel);
   }
 
   @override
