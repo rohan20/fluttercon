aa7af4ec587fc8fec3b7a4dc4077abf5d655cd00
rohan20 <rhntnj@gmail.com>
2023-06-29 15:45:52 +0200

    remove all print statements

--- packages/conference_data/lib/src/domain/repository/conference_data_repository_impl.dart
+++ packages/conference_data/lib/src/domain/repository/conference_data_repository_impl.dart
@@ -16,7 +16,6 @@ class ConferenceDataRepositoryImpl implements ConferenceDataRepository {
   final ConferenceDataRemoteDataSource conferenceDataRemoteDataSource;
   final ConferenceDataLocalDataSource conferenceDataLocalDataSource;
 
-  // TODO(rohan20): Remove print statements.
   @override
   Future<Result<Failure, ConferenceData>> getConferenceData({
     ConferenceDataSource? source = ConferenceDataSource.cached,
@@ -26,32 +25,24 @@ class ConferenceDataRepositoryImpl implements ConferenceDataRepository {
 
       try {
         if (sourceOrFallbackSource == ConferenceDataSource.cached) {
-          print('XXX Getting cached conference data...');
           final cachedConferenceDataModel = await conferenceDataLocalDataSource.getConferenceData();
 
           if (cachedConferenceDataModel.sessions.isNotEmpty) {
-            print('XXX Returning cached conference data because source = cached');
             return Success(ConferenceData.fromConferenceDataModel(cachedConferenceDataModel));
           } else {
-            print('XXX Returning latest conference data because cached sessions were empty');
             return Success(await _getLatestConferenceDataAndCacheIt());
           }
         } else {
-          print('XXX Returning latest conference data because source = latest');
           return Success(await _getLatestConferenceDataAndCacheIt());
         }
       } on ApiClientError {
-        print('XXX Error ApiClientError 1');
         rethrow;
       } catch (e) {
-        print('XXX Returning latest conference data since something went wrong: $e');
         return Success(await _getLatestConferenceDataAndCacheIt());
       }
     } on ApiClientError {
-      print('XXX Error ApiClientError 2');
       return const Error(ServerFailure());
     } catch (e) {
-      print('XXX Error $e');
       return const Error(LocalFailure());
     }
   }
@@ -77,32 +68,24 @@ class ConferenceDataRepositoryImpl implements ConferenceDataRepository {
 
       try {
         if (sourceOrFallbackSource == ConferenceDataSource.cached) {
-          print('XXX Getting cached agenda...');
           final cachedAgendaModel = await conferenceDataLocalDataSource.getAgenda();
 
           if (cachedAgendaModel.sessions.isNotEmpty) {
-            print('XXX Returning cached agenda because source = cached');
             return Success(Agenda.fromAgendaModel(cachedAgendaModel));
           } else {
-            print('XXX Returning latest agenda because cached sessions were empty');
             return Success(await _getLatestAgendaAndCacheIt());
           }
         } else {
-          print('XXX Returning latest agenda because source = latest');
           return Success(await _getLatestAgendaAndCacheIt());
         }
       } on ApiClientError {
-        print('XXX Error ApiClientError 1');
         rethrow;
       } catch (e, stackTrace) {
-        print('XXX Returning latest agenda since something went wrong: $e, $stackTrace');
         return Success(await _getLatestAgendaAndCacheIt());
       }
     } on ApiClientError {
-      print('XXX Error ApiClientError 2');
       return const Error(ServerFailure());
     } catch (e) {
-      print('XXX Error $e');
       return const Error(LocalFailure());
     }
   }
