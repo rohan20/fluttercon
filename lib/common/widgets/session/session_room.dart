c3130f008039803339546d68c1bbb93cd718fee1
rohan20 <rhntnj@gmail.com>
2024-09-14 14:29:02 +0200

    wrap whole widget with Flexible

--- lib/common/widgets/session/session_room.dart
+++ lib/common/widgets/session/session_room.dart
@@ -10,26 +10,28 @@ class SessionRoom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(color: Colors.grey.shade800, width: 0.2),
-      ),
-      child: Row(
-        children: [
-          Icon(Icons.location_on, size: 16, color: Colors.grey.shade400),
-          const SizedBox(width: 4),
-          Flexible(
-            child: Text(
-              roomName,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.bodySmall,
+    return Flexible(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(color: Colors.grey.shade800, width: 0.2),
+        ),
+        child: Row(
+          children: [
+            Icon(Icons.location_on, size: 16, color: Colors.grey.shade400),
+            const SizedBox(width: 4),
+            Flexible(
+              child: Text(
+                roomName,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
