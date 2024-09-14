7537f4b655ccac346b83aface529116a9d55bc81
rohan20 <rhntnj@gmail.com>
2024-09-14 23:35:13 +0200

    update local storage keys and delete legacy stored data

--- lib/features/favourites/data/data_source/favourites_local_data_source_impl.dart
+++ lib/features/favourites/data/data_source/favourites_local_data_source_impl.dart
@@ -4,13 +4,15 @@ import 'package:shared_preferences/shared_preferences.dart';
 class FavouritesLocalDataSourceImpl implements FavouritesLocalDataSource {
   FavouritesLocalDataSourceImpl(this.sharedPreferences);
 
-  static const _favouriteSessionIdsKey = 'favouriteSessionIds';
+  static const _favouriteSessionIdsKey = 'favouriteSessionIds-2024-usa';
 
   final SharedPreferences sharedPreferences;
 
   @override
   Future<void> saveFavouriteSessionIds(List<String> sessionIds) async {
     await sharedPreferences.setStringList(_favouriteSessionIdsKey, sessionIds);
+    // Delete legacy data from older conferences (applicable in case the user didn't uninstall the app between conferences)
+    await sharedPreferences.remove('favouriteSessionIds');
   }
 
   @override
