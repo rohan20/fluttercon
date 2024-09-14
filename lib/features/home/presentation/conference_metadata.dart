b1a81d6a0f2a9607fb6466b4ee6c92f049af2b9e
rohan20 <rhntnj@gmail.com>
2024-09-14 23:30:58 +0200

    update conference dates

--- lib/features/home/presentation/conference_metadata.dart
+++ lib/features/home/presentation/conference_metadata.dart
@@ -1,8 +1,8 @@
 class ConferenceMetadata {
   const ConferenceMetadata._();
 
-  static final DateTime day1 = DateTime(2023, 07, 05, 20);
-  static final DateTime day2 = DateTime(2023, 07, 06, 20);
+  static final DateTime day1 = DateTime(2024, 09, 19, 20);
+  static final DateTime day2 = DateTime(2024, 09, 20, 20);
 
   static const String lightningTalkId = '164900';
   static const String sessionId = '164901';
